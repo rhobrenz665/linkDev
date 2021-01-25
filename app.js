3106657705e5ad54bc13350bc1af2db5ce6dce11
Rhobrenz Buenavista <buenavistarhobrenz@gmail.com>
2021-01-25 15:27:58 +0800

    make it better

--- app.js
+++ app.js
@@ -37,16 +37,6 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 // Error Middleware
 app.use((error, req, res, next) => {
   res.status(error.code || 500);
